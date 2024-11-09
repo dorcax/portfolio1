381217ecb28b828f70836457a186516092e11284
dorcax <dorcasibrahim667@gmail.com>
2024-11-09 22:34:17 +0100

    fixed

--- src/components/Project/projectimage.jsx
+++ src/components/Project/projectimage.jsx
@@ -4,18 +4,20 @@ export default function ProjectImage({name,github,description,demolink,img,alt})
             <div className="image-container">
                 <div className="project-image">
                     <div className="project-text">
-                    
+           
                         <h2>{name}</h2>
                         <p>{description}</p>
                     
                         <div className="demolink">
                             <a href={demolink}>view demo</a>
                             <a href={github}>source code</a>
-                        </div> 
+                        </div>  
+        
                     </div>
-                     <div className="project-image2">
-                        {/* <div></div> */}
-                     <img src={img} alt={alt}  width={200}/>
+                <div className="project-image2"> 
+                    {/* <div className="border"></div>  */}
+                     <img src={img} alt={alt}  />
+                
                      </div>
                     
                     
